c96b27153f9c8155a1559fd827645db8d6e09301
Eric <guitarprewitt@gmail.com>
2013-10-01 16:42:17 -0600

    Ipad Storyboard

---
+++ WorldClock/WorldClock/ZoomViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZoomViewController.h
+//  WorldClock
+//
+//  Created by Eric Prewitt on 10/1/13.
+//  Copyright (c) 2013 Eric Prewitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZoomViewController : UIViewController
+
+@end
---
+++ WorldClock/WorldClock/ZoomViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZoomViewController.m
+//  WorldClock
+//
+//  Created by Eric Prewitt on 10/1/13.
+//  Copyright (c) 2013 Eric Prewitt. All rights reserved.
+//
+
+#import "ZoomViewController.h"
+
+@interface ZoomViewController ()
+
+@end
+
+@implementation ZoomViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
