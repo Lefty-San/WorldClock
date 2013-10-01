4813dc4fccbb3f5c89d3323173ba8fba28b9705d
Eric <guitarprewitt@gmail.com>
2013-10-01 15:53:20 -0600

    projecto

---
+++ WorldClock/WorldClock/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WorldClock
+//
+//  Created by Eric Prewitt on 10/1/13.
+//  Copyright (c) 2013 Eric Prewitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WorldClock/WorldClock/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WorldClock
+//
+//  Created by Eric Prewitt on 10/1/13.
+//  Copyright (c) 2013 Eric Prewitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
